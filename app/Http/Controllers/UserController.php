8c5addc09ae396cd251fabc6c9bf50a3a2ebe4cc
KofiEric <ericp@menufinderafrica.com>
2022-08-25 00:23:03 -0700

    created GET route for user ID url

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function userview()
+    public function userview($id)
     {
         // dd('User view ?');
         return view('user_card');
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user', [UserController::class, 'userview'])->name('user');
+Route::get('/user/{$id}', [UserController::class, 'userview'])->name('user');
